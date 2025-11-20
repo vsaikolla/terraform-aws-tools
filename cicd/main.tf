a64ffb2b79ed7101badae0f2ed654ef4f2752873
sainath <kollavenkatasai999@gmail.com>
2025-11-20 12:42:01 -0500

    nexus

--- cicd/main.tf
+++ cicd/main.tf
@@ -45,12 +45,11 @@ module "nexus" {
   subnet_id = "subnet-0e33ccaf834d4a0c6"
   ami = data.aws_ami.nexus_ami_info.id
   key_name = aws_key_pair.tools.key_name
-  root_block_device = [
-    {
+  root_block_device = {
       volume_type = "gp3"
       volume_size = 30
-    }
-  ]
+  }
+  
   tags = {
     Name = "nexus"
   }
